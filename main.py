678def01f289867651df28978ff91c2e1454998f
nfarraro <nfarraro@gmail.com>
2026-04-17 07:10:13 -0600

    cap fps

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -17,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
