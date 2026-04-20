6ab4bd43c27058584ce58ca660d4d5ae168250b9
nfarraro <nfarraro@gmail.com>
2026-04-20 08:18:09 -0600

    add up down move and sprite groups

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    clock = pygame.time.Clock()
-    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
